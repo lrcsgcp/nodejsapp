2420b4cfc1c6812fa655a38d97669776c717bc3b
lrcsgcp <153192133+lrcsgcp@users.noreply.github.com>
2025-06-12 19:25:01 +0200

    Update nodejsDSL.groovy
    cambio ultima hora

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -1,9 +1,9 @@
 job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
-    scm {
-        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node ->
-            node / gitConfigName('macloujulian')
-            node / gitConfigEmail('macloujulian@gmail.com')
+       scm {
+         git('https://github.com/su_nombre_de_usuario/nodejsapp.git', 'master') { node ->
+             node / gitConfigName('su_nombre_de_usuario')
+             node / gitConfigEmail('el_mail_de_su_cuenta_de_GitHub')
         }
     }
     triggers {
