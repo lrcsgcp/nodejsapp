872e1213ae4dc12efb64924912322b87d6b8f5c3
lrcsgcp <153192133+lrcsgcp@users.noreply.github.com>
2025-06-12 22:03:35 +0200

    Update nodejsDSL.groovy
    añadir name y email

--- DSL/nodejsDSL.groovy
+++ DSL/nodejsDSL.groovy
@@ -2,8 +2,8 @@ job('Aplicacion Node.js DSL') {
     description('Aplicación Node JS DSL para el curso de Jenkins')
        scm {
          git('https://github.com/su_nombre_de_usuario/nodejsapp.git', 'master') { node ->
-             node / gitConfigName('su_nombre_de_usuario')
-             node / gitConfigEmail('el_mail_de_su_cuenta_de_GitHub')
+             node / gitConfigName('lrcsgcp')
+             node / gitConfigEmail('lrcsgcp@gmail.com')
         }
     }
     triggers {
