cb75bd0f3f4224f6a128d415a0d82550de74123b
lrcsgcp <153192133+lrcsgcp@users.noreply.github.com>
2025-06-12 22:07:04 +0200

    Update nodejsDockerDSL.groovy
    datos repositorio

--- DSL/nodejsDockerDSL.groovy
+++ DSL/nodejsDockerDSL.groovy
@@ -2,8 +2,8 @@ job('Aplicacion Node.js DSL') {
         description('Aplicación Node JS DSL para el curso de Jenkins')
         scm {
             git('https://github.com/su_nombre_de_usuario/nodejsapp.git', 'master') { node ->
-                node / gitConfigName('su_nombre_de_usuario')
-                node / gitConfigEmail('el_mail_de_su_cuenta_de_GitHub')
+                node / gitConfigName('lrcsgcp')
+                node / gitConfigEmail('lrcsgcp@gmail.com')
             }
         }
      
@@ -15,9 +15,9 @@ job('Aplicacion Node.js DSL') {
         }
         steps {
             dockerBuildAndPublish {
-                repositoryName('su_nombre_de_usuario/nodejsapp')
+                repositoryName('lrcsgcp/nodejsapp')
                 tag('${GIT_REVISION,length=7}')
-                registryCredentials('El ID con el que hayan guardado la credencial de su cuenta de docker en jenkins')
+                registryCredentials('luirramon')
                 forcePull(false)
                 createFingerprints(false)
                 skipDecorate()
